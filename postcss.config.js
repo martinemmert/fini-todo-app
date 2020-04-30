347df519af3401ce2f8f534858df81d57c3a3cc0
Martin Emmert <martin@martinemmert.de>
2020-04-30 15:42:28 +0200

    chore: update tailwind and fix postcss config for nextjs

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,3 @@
-const tailwind = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const presetEnv = require("postcss-preset-env");
-
-const purge = purgecss({
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
-const plugins =
-  process.env.NODE_ENV === "production"
-    ? [tailwind, purge, presetEnv]
-    : [tailwind, presetEnv];
-
 module.exports = {
-  plugins,
+  plugins: { "tailwindcss": {}, "postcss-preset-env": {} },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   // plugins: [require("@tailwindcss/custom-forms")],
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     spacing: {
       0: "0",
