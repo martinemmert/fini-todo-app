2fb57f8a1a20175665397a4797d62591872e9c19
Martin Emmert <martin@martinemmert.de>
2020-05-01 14:32:52 +0200

    style(components): add minor style adjustments to ProgressBar and TaskListProgress

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -8,7 +8,7 @@ const ProgressBar: ProgressBarComponent = ({ progress = 0 }) => {
   return (
     <div className="w-full h-1 overflow-hidden bg-gray-200 rounded-full">
       <div
-        className="w-full h-full transition-transform duration-150 ease-out origin-left bg-teal-400"
+        className="w-full h-full transition-transform duration-150 ease-out origin-left transform scale-x-0 bg-teal-400"
         style={{ transform: `scaleX(${progress})` }}
       />
     </div>
--- components/TaskListProgress/TaskListProgress.tsx
+++ components/TaskListProgress/TaskListProgress.tsx
@@ -4,11 +4,12 @@ import ProgressBar from "../ProgressBar";
 type TaskListProgressComponent = React.FC<{
   completedTasks: number;
   totalTasks: number;
-}>;
+} & React.HTMLAttributes<HTMLDivElement>>;
 
 const TaskListProgress: TaskListProgressComponent = ({
   completedTasks,
   totalTasks,
+  ...props
 }) => {
   const pendingTasks = totalTasks - completedTasks;
   const progress = completedTasks / totalTasks;
@@ -18,9 +19,9 @@ const TaskListProgress: TaskListProgressComponent = ({
   const progressLabel = `${completedTasks} of ${totalTasks} completed${pendingTasks > 0 ? pendingLabel : ''}`;
   const progressPercentageLabel = `${Math.round(progress * 100)}% completed`;
   return (
-    <div>
+    <div {...props}>
       <ProgressBar progress={progress} />
-      <p className="mt-2 font-sans leading-none uppercase text-small">
+      <p className="mt-2 font-sans leading-none text-gray-600 uppercase text-small">
         <span className="inline-block w-1/2">{progressLabel}</span>
         <span className="inline-block w-1/2 text-right">
           {progressPercentageLabel}
